249086289ff7114496a83d0ee729447855ebcb88
Alex Boyd <alexanderleeboyd@gmail.com>
2022-07-29 17:14:23 -0400

    Complete US_2

--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -62,4 +62,27 @@ RSpec.describe 'customer show page' do
     expect(page).to_not have_content("Whole Foods")
   end
 
+  it 'displays total price of all the customer\'s items' do
+    supermarket1 = Supermarket.create!(name: "Trader Joes", location: "DC")
+    supermarket2 = Supermarket.create!(name: "Whole Foods", location: "New York")
+
+    customer1 = Customer.create!(name: "Alex", supermarket_id: supermarket1.id)
+    customer2 = Customer.create!(name: "Kiara", supermarket_id: supermarket1.id)
+
+    item1 = Item.create!(name: "Pretzels", price: 100)
+    item2 = Item.create!(name: "Ice Cream", price: 50)
+    item3 = Item.create!(name: "Salad", price: 10)
+    item4 = Item.create!(name: "Chips", price: 5)
+
+    customer_item1 = CustomerItem.create!(customer_id: customer1.id, item_id: item1.id)
+    customer_item2 = CustomerItem.create!(customer_id: customer1.id, item_id: item2.id)
+    customer_item3 = CustomerItem.create!(customer_id: customer1.id, item_id: item3.id)
+
+    visit "/customers/#{customer1.id}"
+
+    within "#total-price" do
+      expect(page).to have_content("Total Price: 160")
+    end
+  end
+
 end
